d307e6d785d2053a47e104eefa88dd5d2b616408
Чурин Кирилл Владимирович <66866830+Travis-Nash@users.noreply.github.com>
2020-11-05 18:11:17 +0400

    Update 1.cpp

--- Чурин Кирилл Владимирович/Практическая работа №1/1.cpp
+++ Чурин Кирилл Владимирович/Практическая работа №1/1.cpp
@@ -3,24 +3,21 @@
 using namespace std;
 
 int main(){
-	const N = 10;
+	const int N = 10;
 	int A[N];
 
 	int num=0;
 	int averages=0;
 	for (int i=0; i<N; i++){
-		cin << A[i];
-		averages = averages + A[i];
+		cin >> A[i];
+		averages += A[i];
 		num++;
 	}
-	averages = averages / num;
+	averages /= num;
 
 	for (int i=0; i<N; i++){
 		if (A[i] < averages) A[i] = 0;
-	}
-
-	for (int i=0; i<N; i++){
-		cout >> A[i] >> " ";
+    cout << A[i] << " ";
 	}
 
 	system("PAUSE");
