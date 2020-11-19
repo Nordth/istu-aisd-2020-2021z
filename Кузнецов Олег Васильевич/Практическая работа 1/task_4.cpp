c61232e20d5a84775f2d1fb02f58ad01f86b1ab0
OlegKyz <oleg.kyznetzov@mail.ru>
2020-11-19 19:03:36 +0400

    corrected mistakes in 4 task

--- Кузнецов Олег Васильевич/Практическая работа 1/task_4.cpp
+++ Кузнецов Олег Васильевич/Практическая работа 1/task_4.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
+const int values_number = 10;
 int arr[] = {9, 7, 9, 8, 6, 6, 3, 1, 2, 0, 2};
-int number_repeat[10];
+int number_repeat[values_number];
 
 int main()
 {
@@ -9,7 +10,7 @@ int main()
 	for (int i = 0; i < arr_size; ++i) {
 		number_repeat[arr[i]]++;
 	}
-	for (int i = 0; i <= 9; ++i) {
+	for (int i = 0; i <= values_number; ++i) {
 		for (int j = 0; j < number_repeat[i]; ++j) {
 			std::cout << i << " ";
 		}
