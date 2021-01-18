9e3d5eb403c800c9ce914469760b734cc26cc38c
AlexandrBakirov <evening.dreams@yandex.ru>
2021-01-18 19:43:35 +0400

    Create 4 Задача.cs
    Задача № 4. Практическая работа №1.

---
+++ Бакиров Александр Владимирович/Практическая работа №1/4 Задача.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Zadacha4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 5, 7, 9, 4, 5, 5, 1, 2, 5, 4, 5, 8, 5 };
+            int[] reps = new int[10];
+            int k = 0;
+
+            for (int i = 0; i != arr.Length; i++)
+            {
+                reps[arr[i]]++;
+            }
+
+            for (int i = 0; i != 10; i++)
+            {
+                if (reps[i] > 0)
+                {
+                    Console.WriteLine(i + " - " + reps[i] + " повторов" + '\n');
+                }
+            }
+
+            for(var i= 0; i!=reps.Length;i++)
+            {
+                while (reps[i] != 0)
+
+                {
+                    arr[k] = i;
+                    k++;
+                    reps[i]--;
+                }
+            }
+            Console.Write('\n' + "Source array " + arr[0]);
+
+            for (var i = 0; i != arr.Length; i++)
+                Console.Write (", " + arr[i]);
+            Console.ReadKey();
+        }
+    }
+}
