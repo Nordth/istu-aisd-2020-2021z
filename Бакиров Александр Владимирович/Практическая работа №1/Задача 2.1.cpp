b8d017c76384567d635d0c02895e8efc5a1073be
AlexandrBakirov <evening.dreams@yandex.ru>
2021-01-18 18:51:42 +0400

    Update Задача 2.1.cpp
    Заменил все if-ы на switch-и

--- Бакиров Александр Владимирович/Практическая работа №1/Задача 2.1.cpp
+++ Бакиров Александр Владимирович/Практическая работа №1/Задача 2.1.cpp
@@ -6,17 +6,19 @@ int main()
 	float a, b, result=0;
 	char c;
 	cin >> a >> c >> b;
-	if (c == '+')
-		result = a + b;
-	else
-		if (c == '-')
-			result = a - b;
-		else
-			if (c == '*')
-				result = a * b;
-			else
-				if (c == '/')
-					result = a / b;
-			
-	cout << result;
+	switch (c)
+	{
+		case '+': 
+			cout<<a+b;
+			break;
+		case '-':
+			cout<<a-b;
+			break;
+		case '*':
+			cout<<a*b;
+			break;
+		case '/':
+			cout<<a/b;
+			break;
+	}	
 }
