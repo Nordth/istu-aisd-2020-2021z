18c055ce4bd4234114a68b13ad24085208e0c414
shokedjobana <shokedjobana@gmail.com>
2021-01-27 00:30:26 +0400

    Update Однонаправленный циклический цикл.cpp

--- lab/Однонаправленный циклический цикл.cpp
+++ lab/Однонаправленный циклический цикл.cpp
@@ -8,42 +8,42 @@ int main(){
 	while (imenu != 5) {
 		system("cls");
 		//array output
-		if ( mass.empty() ) {std::cout << "Массив пустой!!!";
+		if ( mass.empty() ) {std::cout << "Empty array!!!";
 		}else{
-			std::cout << "Массив: { ";
+			std::cout << "Array: { ";
 			for (int i = 0; i != mass.size(); i++) { std::cout << mass[i] << ", "; }
 			std::cout << "}";
 		}std::cout << std::endl;
 
 		//menu output
-		std::cout << std::endl << "1. Добавить значение в конец." << std::endl << "2. Вставить после." << std::endl << "3. Получить значение по индексу." << std::endl << "4. Удаление элемента по значению." << std::endl << "5. Выход." << std::endl << ">> ";
+		std::cout << std::endl << "1. Add value to end." << std::endl << "2. Enter after." << std::endl << "3. Get value by index." << std::endl << "4. Removing element by value." << std::endl << "5. Exit." << std::endl << ">> ";
         std::cin >> imenu;
 
 		//menu function
         switch (imenu) {
 		case 1:
 			system("cls");
-			std::cout << "число: ";
+			std::cout << "number: ";
 			std::cin >> number1;
 			mass.push_back(number1);
 			break;
 		case 2:
 			system("cls");
-			std::cout << "число: ";
+			std::cout << "number: ";
 			std::cin >> number1;
-			std::cout << "вставить в: ";
+			std::cout << "put in: ";
 			std::cin >> number2;
 			mass.emplace(mass.begin()+number2+1, number1);
 			break;
 		case 3: 
 			system("cls");
-			std::cout << "индекс: ";
+			std::cout << "index: ";
 			std::cin >> number1;
 			std::cout << mass[number1-1] << std::endl;
 			system("pause");
 			break;
 		case 4: 
-			std::cout << "число: ";
+			std::cout << "number: ";
 			std::cin >> number1;
 			for (int i = 0; i != mass.size(); i++) {
 				//delete if finded
