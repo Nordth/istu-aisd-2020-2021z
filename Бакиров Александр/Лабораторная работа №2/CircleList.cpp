72731d70efa8257b2cf676f13e01f8bed8f2e317
AlexandrBakirov <evening.dreams@yandex.ru>
2021-02-03 20:30:51 +0400

    Лабораторная работа №2
    Вариант 3

---
+++ Бакиров Александр/Лабораторная работа №2/CircleList.cpp
@@ -0,0 +1,221 @@
+﻿
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+class CircleList
+{
+public:
+	CircleList();
+	/*
+	* Конструктор для очисти памяти
+	*/
+	~CircleList();
+
+	void AddToEnd(int data); // вставка элемента в конец списка
+	void Print(); // печать всех элементов
+	void AddOnIndex(int index, int data); // Добавление элемента в список по индексу
+	void DeleteValue(int data); // удаление элемента по его значению
+
+	// получение значения элемента по индексу
+	 void SearchNum(int index);
+
+	// структура узла списка
+	struct Node
+	{
+		int data; // поле данных
+		struct Node* next;// указатель на следующий элемент
+	};
+	// Указатели на первый и последующий элементы списка
+	Node* Head;
+	Node* Tail;
+	//переменная для фиксации кол-ва элементов
+	int count;
+
+};
+
+CircleList::CircleList() //изначальное значение элементов списка - NULL
+{
+	Head = Tail = NULL;
+	count = 0;
+}
+
+
+void CircleList::AddToEnd(int data)
+{
+	// Выделение памяти для нового элемента
+	Node* temp = new Node;
+	temp->data = data;
+
+	// Если присутствует первый элемент
+	if (Head != NULL)
+	{
+		temp->next = Head;
+		Tail->next = temp;
+		Tail = temp;
+	}
+	// Если первого элемента нет, то нужно его создать
+	else
+	{
+		Tail = Head = temp;
+		temp->next = Head;
+	}
+	count++;
+	cout << "\nЧисло " << data << " добавлено.";
+}
+
+
+void CircleList::DeleteValue(int data)
+{
+	if (Head->data == data) // Если удаляем первый элемент
+	{
+		Node* temp = Head;
+		Head = Head->next;
+		Tail->next = Head;
+		delete temp;
+		count--;
+	}
+	else
+	{
+		Node** temp2 = &Head->next; // создаем ссылку на выделяемую область памяти для указателя элемента идущего за первым
+		while (*temp2 != Head) // пробегаем по списку в поисках удаляемого значения
+		{
+			if ((*temp2)->data == data)
+			{
+				// переприсваиваем указатели перед удалением
+				Node* temp = *temp2;
+				*temp2 = (*temp2)->next;
+				delete temp;
+				count--;
+			}
+			temp2 = &(*temp2)->next;
+		}
+	}
+	cout << "\nЭлемент " << data << " удален из списка.";
+}
+
+
+void CircleList::AddOnIndex(const int index, const int data)
+{
+	// Если список пуст или индекс больше общего кол-ва элементов списка, то добавить новое значение в список
+	if (Head == NULL || index > count)
+	{
+		AddToEnd(data);
+		return;
+	}
+	// создание ссылки на переменную temp  с типом Node*(для того,чтобы temp существовала не только в теле метода AddOnIndex)
+	// как будто это глобальная переменная, т.е указатель на конкретную область памяти, а не команда для выделения нового участка
+	// информация https://coderoad.ru/12124096
+	Node** temp = &Head;
+
+	for (int x = 1; x < index; ++x)
+	{
+		temp = &((*temp)->next);
+	}
+	// создание новой области памяти
+	Node* t = new Node;
+	// передача в нее значения для вставки
+	t->data = data;
+	// создание указателя на ссылку в области памяти
+	t->next = *temp;
+	// вставка элемента
+	*temp = t;
+	count++;
+	cout << "\nЧисло " << data << " добавлено по индесу " << index;
+}
+
+
+void CircleList::SearchNum(int index)
+{
+	Node* temp;
+	temp = Head;
+	if (Head == NULL)
+	{
+		cout << "Лист пуст" << endl;
+		return;
+	}
+	else if (index>count)
+	{
+		cout << "\nПо индексу " << index << " элементов не существует";
+	}
+	else
+	{
+		for (int i = 0; i != index; i++)
+		{
+			temp = temp->next;
+		}
+		cout << "\nЭлемент " << temp->data << " располагается по индексу " << index;
+	}
+	
+}
+
+
+CircleList::~CircleList()
+{
+	Tail->next = nullptr;
+	while (Head != NULL) {
+		Tail = Head;
+		Head = Head->next;
+		delete Tail;
+	}
+	Tail = nullptr;
+	cout << "Память очищена" << endl;
+}
+
+void CircleList::Print()
+{
+	cout << endl << endl;
+	if (Head == NULL)
+	{
+		cout << "Список пуст";
+		return;
+	}
+	else
+	{
+		cout << Head->data << " ";
+		Node* temp = Head->next;
+		do
+		{
+			cout << temp->data << " ";
+			temp = temp->next;
+
+		} while (temp != Head);	
+	}
+	cout << endl;
+}
+
+int main()
+{
+	
+
+	CircleList* circlelist = new CircleList();
+	setlocale(LC_ALL, "Russian");
+	circlelist->AddToEnd(4);
+	circlelist->AddToEnd(5);
+	circlelist->AddToEnd(5);
+	circlelist->Print();
+	circlelist->~CircleList();
+	circlelist->AddToEnd(7);
+	circlelist->AddToEnd(8);
+	circlelist->AddToEnd(9);
+	circlelist->AddToEnd(10);
+	circlelist->AddToEnd(5);
+	circlelist->AddToEnd(3);
+	circlelist->AddToEnd(1);
+	circlelist->Print();
+	circlelist->DeleteValue(9);
+	circlelist->Print();
+	circlelist->DeleteValue(7);
+	circlelist->Print();
+	circlelist->AddOnIndex(1, 12);
+	circlelist->AddOnIndex(2, 13);
+	circlelist->DeleteValue(12);
+	circlelist->Print();
+	circlelist->SearchNum(10);
+	circlelist->SearchNum(2);
+	
+}
+
+
---
+++ Бакиров Александр/Лабораторная работа №2/Stack.cpp
@@ -0,0 +1,110 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int top;// врехний элемент стека
+
+
+void DoubleArr(int Stack[], int count)
+{
+	int* BiggerStack = new int[count*2];
+	for (int i = 0; i < count; i++)
+		BiggerStack[i] = Stack[i];
+}
+
+void Push(int Stack[], int count)
+{
+	int data;
+	cout << "Введите число, которое будет добавлено в стек ";
+	cin >> data;
+
+	if (top == (count - 1))
+	{
+		DoubleArr(Stack, count);
+	}
+	else
+	{
+		++top;
+		Stack[top] = data;
+	}
+	cout << "Элемент " << data << " добавлен в стек\n";
+}
+
+void Print(int Stack[])
+{
+
+	if (top == -1 || top < -1)
+	{
+		cout << "Стек пуст!";
+	}
+	else
+	{
+		cout << "Стек: ";
+		for (int i = top; i >= 0; --i)
+		{
+			cout << Stack[i] << " ";
+		}
+	}
+	cout << "\n";
+}
+void Pop(int Stack[])
+{
+	int deleteValue;
+
+	// Если стек пуст, то удалять нечего
+	if (top == -1)
+	{
+		deleteValue = 0;
+		cout << "Стек пуст!!!";
+	}
+	else
+	{
+		deleteValue = Stack[top];
+		--top;
+	}
+	cout << "Элемент хранящийся на вершине стека: " << deleteValue<<"\n";
+	cout << "Стек после удаления ";
+	Print(Stack);
+}
+
+void Copy(int Stack[])//Клонирую стек путем выделения новой памяти и занесения в нее всех значений из оригинального стека
+{
+	cout << "Копирую стек ... ";
+	int* copiedStack = Stack;
+
+	cout << "\nКопия стека выглядит так: \n";
+	Print(copiedStack);
+}
+
+// Очистка памяти от стека
+void ClearMem(int Stack[])
+{
+	cout << "Удаляю стек... ";
+	delete[] Stack;
+	top = -1;
+	cout << "\nПамять очищена!\n";
+}
+
+
+int main()
+{
+	top = -1; // изначальное значение положения первого элемента. Не 0, т.к. реализуем на базе массива
+	int count = 10; // количество элементов стека.
+	int data; //значение элемента стека.
+
+	setlocale(LC_ALL, "Russian");
+
+	int* Stack = new int[count]; // создание стека на базе массива размером count.
+
+//	"------------------------------------------------------------------------------------------------"
+	Push(Stack,count);
+	Push(Stack,count);
+	Push(Stack,count);
+	Print(Stack);
+	Pop(Stack);
+	Copy(Stack);
+	ClearMem(Stack);
+	Push(Stack, count);
+	Print(Stack);
+	return 0;
+}
