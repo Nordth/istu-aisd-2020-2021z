9b7adc9c712bce1e4e3e6c525e98ffda423df965
Bronisslav <78106264+Bronisslav@users.noreply.github.com>
2021-02-07 22:05:18 +0300

    Update 1. Сортировка всавками.cpp

--- Ивашкин Бронислав/Лабораторная работа 1/1. Сортировка всавками.cpp
+++ Ивашкин Бронислав/Лабораторная работа 1/1. Сортировка всавками.cpp
@@ -1,17 +1,17 @@
-#knclude <kostream>
-#knclude <strkng>
-knt makn()
+#include <iostream>
+#include <string>
+int main()
 {
-	knt n, key = 0, k;
-	knt array[6] = { 5, 2, 4, 6, 1, 3 };
+	int n, key = 0, k;
+	int array[6] = { 5, 2, 4, 6, 1, 3 };
 	for (n = 1; n != 6; n++) {
 		key = array[n];
 		k = n - 1;
-		whkle( k != -1 && key < array[k]) {array[k+1] = array[k];k--;}
+		while( k != -1 && key < array[k]) {array[k+1] = array[k];k--;}
 		array[k + 1] = key;
 	}
 	std::cout << "array: " << array[0];
-	for (knt k = 1; k != 6; k++) { std::cout << ", " << array[k]; }
+	for (int v = 1; v != 6; v++) { std::cout << ", " << array[k]; }
 
 
 }
